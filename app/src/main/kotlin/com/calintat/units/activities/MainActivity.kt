0ca326a837025024a4376fdb9566d17534ef7fd3
calintat <calintat@gmail.com>
2017-03-29 00:33:59 +0300

    Support for dynamic shortcuts

--- app/src/main/kotlin/com/calintat/units/activities/MainActivity.kt
+++ app/src/main/kotlin/com/calintat/units/activities/MainActivity.kt
@@ -3,9 +3,9 @@ package com.calintat.units.activities
 import android.content.ClipData
 import android.content.ClipboardManager
 import android.content.Context
-import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.support.annotation.IdRes
 import android.support.customtabs.CustomTabsIntent
 import android.support.v4.content.ContextCompat
 import android.support.v7.app.AppCompatActivity
@@ -13,28 +13,29 @@ import android.support.v7.app.AppCompatDelegate
 import android.support.v7.widget.DividerItemDecoration
 import android.support.v7.widget.LinearLayoutManager
 import android.text.Editable
-import android.text.TextWatcher
-import android.widget.Toast
+import android.widget.EditText
 import com.calintat.units.R
 import com.calintat.units.recycler.Adapter
 import com.calintat.units.utils.Converter
-import com.calintat.units.utils.PreferenceUtils
+import com.calintat.units.utils.ShortcutUtils
+import com.github.calintat.Alps
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlin.properties.Delegates
+import org.jetbrains.anko.configuration
+import org.jetbrains.anko.startActivity
+import org.jetbrains.anko.textChangedListener
+import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
 
-    private val KEY_QUANTITY = "com.calintat.units.KEY_QUANTITY"
+    private val KEY_ID = "com.calintat.units.KEY_ID"
 
-    private val KEY_POSITION = "com.calintat.units.KEY_POSITION"
+    private val adapter by lazy { Adapter(this) }
 
-    private val QUICK_ACCESS = "com.calintat.units.QUICK_ACCESS"
+    private var id: Int? = null
 
-    var adapter = Adapter(this)
+    private var position: Int? = null
 
-    var position by Delegates.notNull<Int>()
-
-    var quantity by Delegates.notNull<Converter.Quantity>()
+    private var favourites = emptySet<Int>()
 
     //----------------------------------------------------------------------------------------------
 
@@ -44,33 +45,42 @@ class MainActivity : AppCompatActivity() {
 
         setContentView(R.layout.activity_main)
 
+        Alps.init(this)
+
         setTheme()
 
         setToolbar()
 
-        setConverter()
-
-        setRecyclerView()
+        setMainContent()
 
         setNavigationView()
 
-        restore(savedInstanceState)
+        init(savedInstanceState)
     }
 
     override fun onResume() {
 
         super.onResume()
 
-        updateRecyclerView()
+        refreshActionMenu()
 
-        updateNavigationView()
+        refreshRecyclerView()
+
+        refreshNavigationView()
+    }
+
+    override fun onPause() {
+
+        super.onPause()
+
+        updateShortcuts()
     }
 
     override fun onBackPressed() {
 
-        when {
+        when (drawerLayout.isDrawerOpen(navigationView)) {
 
-            drawerLayout.isDrawerOpen(navigationView) -> drawerLayout.closeDrawers()
+            true -> drawerLayout.closeDrawers()
 
             else -> super.onBackPressed()
         }
@@ -78,18 +88,62 @@ class MainActivity : AppCompatActivity() {
 
     override fun onSaveInstanceState(outState: Bundle) {
 
-        outState.putInt(KEY_QUANTITY, quantity.id)
-
-        outState.putInt(KEY_POSITION, position)
+        id?.let { outState.putInt(KEY_ID, it) }
 
         super.onSaveInstanceState(outState)
     }
 
     //----------------------------------------------------------------------------------------------
 
+    private fun init(savedInstanceState: Bundle?) {
+
+        configuration(fromSdk = 25) { favourites = ShortcutUtils.getShortcuts(this) }
+
+        val defaultId = Alps.getInt(KEY_ID, R.id.navigation_length)
+
+        if (savedInstanceState == null) { /* opened from launcher or app shortcut */
+
+            selectId(ShortcutUtils.retrieveId(intent.action) ?: defaultId)
+        }
+        else { /* orientation change, activity resumed, etc */
+
+            selectId(savedInstanceState.getInt(KEY_ID, defaultId))
+        }
+    }
+
+    private fun selectId(@IdRes id: Int) {
+
+        this.id = id
+
+        Alps.putInt(KEY_ID, id)
+
+        refreshActionMenu()
+
+        val item = Converter.get(id)
+
+        adapter.units = item.units
+
+        toolbar.setBackgroundResource(item.color)
+
+        drawerLayout.setStatusBarBackground(item.colorDark)
+
+        selectPosition(0)
+    }
+
+    private fun selectPosition(position: Int) {
+
+        this.position = position
+
+        refreshRecyclerView()
+
+        textView1.setText(adapter.units[position].name)
+
+        textView2.setText(adapter.units[position].symbol)
+    }
+
     private fun setTheme() {
 
-        when (PreferenceUtils.getBoolean(this, "pref_dark_theme", false)) {
+        when (Alps.getBoolean("pref_dark_theme")) {
 
             true -> AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
 
@@ -101,37 +155,32 @@ class MainActivity : AppCompatActivity() {
 
         toolbar.inflateMenu(R.menu.action)
 
-        toolbar.setOnMenuItemClickListener { editText.text.clear(); true }
+        toolbar.setOnMenuItemClickListener {
+
+            when (it.itemId) {
+
+                R.id.action_remove_from_favourites -> { removeFromFavourites(); true }
+
+                R.id.action_add_to_favourites -> { addToFavourites(); true }
+
+                R.id.action_clear_input -> { clearInput(); true }
+
+                else -> false
+            }
+        }
 
         toolbar.setNavigationIcon(R.drawable.ic_action_menu)
 
         toolbar.setNavigationOnClickListener { drawerLayout.openDrawer(navigationView) }
+
+        toolbar.overflowIcon = ContextCompat.getDrawable(this, R.drawable.ic_overflow)
     }
 
-    private fun setConverter() {
+    private fun setMainContent() {
 
-        editText.addTextChangedListener(object : TextWatcher {
+        editText.afterTextChanged { refreshActionMenu(); refreshRecyclerView() }
 
-            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
-
-                // do nothing
-            }
-
-            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
-
-                // do nothing
-            }
-
-            override fun afterTextChanged(s: Editable?) {
-
-                updateRecyclerView()
-            }
-        })
-    }
-
-    private fun setRecyclerView() {
-
-        adapter.onClick = { updateSelection(it) }
+        adapter.onClick = { selectPosition(it) }
 
         adapter.onLongClick = { copyToClipboard(it) }
 
@@ -139,7 +188,7 @@ class MainActivity : AppCompatActivity() {
 
         recyclerView.layoutManager = LinearLayoutManager(this)
 
-        recyclerView.addItemDecoration(DividerItemDecoration(this, LinearLayoutManager.VERTICAL))
+        recyclerView.addItemDecoration(DividerItemDecoration(this, DividerItemDecoration.VERTICAL))
     }
 
     private fun setNavigationView() {
@@ -152,102 +201,92 @@ class MainActivity : AppCompatActivity() {
 
             when (it.itemId) {
 
-                R.id.navigation_settings -> gotoSettings()
+                R.id.navigation_settings -> startActivity<SettingsActivity>()
 
                 R.id.navigation_feedback -> gotoFeedback()
 
-                else -> updateSelection(Converter.get(it.itemId))
+                else -> selectId(it.itemId)
             }
 
             true
         }
     }
 
-    //----------------------------------------------------------------------------------------------
+    private fun clearInput() = editText.text.clear()
 
-    fun restore(savedInstanceState: Bundle?) {
+    private fun addToFavourites() = id?.let {
 
-        if (savedInstanceState == null) { // opened from launcher or app shortcut
+        if (favourites.size < 4) { /* can add to favourites */
 
-            val defId = PreferenceUtils.getInt(this, QUICK_ACCESS, R.id.navigation_length)
+            favourites += it
 
-            val quantity = Converter.getShortcut(intent.action, defId)
+            refreshActionMenu()
 
-            updateSelection(quantity)
+            toast(R.string.msg_add_to_favourites)
         }
-        else { // orientation change, etc
+        else { /* error: cannot have more than 4 favourites */
 
-            val quantity = Converter.get(savedInstanceState.getInt(KEY_QUANTITY))
-
-            val position = savedInstanceState.getInt(KEY_POSITION)
-
-            updateSelection(quantity, position)
+            toast(R.string.err_add_to_favourites)
         }
     }
 
-    //----------------------------------------------------------------------------------------------
+    private fun removeFromFavourites() = id?.let {
 
-    fun updateRecyclerView() {
+        favourites -= it
 
-        val num = editText.text.toString().safeToDouble()
+        refreshActionMenu()
 
-        adapter.input = quantity.units[position].selfToBase(num)
+        toast(R.string.msg_remove_from_favourites)
     }
 
-    fun updateNavigationView() {
+    private fun refreshActionMenu() {
 
-        val preference = PreferenceUtils.getBoolean(this, "pref_advanced")
+        val contained = favourites.contains(id)
 
-        navigationView.menu.setGroupVisible(R.id.advanced, preference)
+        toolbar.menu.findItem(R.id.action_clear_input).isVisible = editText.text.isNotEmpty()
+
+        toolbar.menu.findItem(R.id.action_add_to_favourites).isVisible = !contained
+
+        toolbar.menu.findItem(R.id.action_remove_from_favourites).isVisible = contained
     }
 
-    fun updateSelection(position: Int) {
+    private fun refreshRecyclerView() {
 
-        this.position = position
+        val num = editText.text.toString().toDoubleOrNull() ?: Double.NaN
 
-        updateRecyclerView()
-
-        textView1.setText(quantity.units[position].name)
-
-        textView2.setText(quantity.units[position].symbol)
+        position?.let { adapter.input = adapter.units[it].selfToBase(num) }
     }
 
-    fun updateSelection(quantity: Converter.Quantity, position: Int = 0) {
+    private fun refreshNavigationView() {
 
-        this.quantity = quantity
-
-        adapter.units = quantity.units
-
-        updateSelection(position)
-
-        toolbar.setBackgroundResource(quantity.color)
-
-        drawerLayout.setStatusBarBackground(quantity.colorDark)
-
-        PreferenceUtils.putInt(this, QUICK_ACCESS, quantity.id)
+        navigationView.menu.setGroupVisible(R.id.advanced, Alps.getBoolean("pref_advanced"))
     }
 
-    //----------------------------------------------------------------------------------------------
-
-    fun gotoSettings() = startActivity(Intent(this, SettingsActivity::class.java))
-
-    fun gotoFeedback() {
+    private fun gotoFeedback() {
 
         val builder = CustomTabsIntent.Builder()
 
-        builder.setToolbarColor(ContextCompat.getColor(this, quantity.color))
+        id?.let { builder.setToolbarColor(ContextCompat.getColor(this, Converter.get(it).color)) }
 
         builder.build().launchUrl(this, Uri.parse("https://github.com/calintat/Units/issues"))
     }
 
-    fun copyToClipboard(str: String) {
+    private fun copyToClipboard(text: String) {
 
-        val c = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
+        val clipboardManager = getSystemService(Context.CLIPBOARD_SERVICE) as ClipboardManager
 
-        c.primaryClip = ClipData.newPlainText("conversion output", str)
+        clipboardManager.primaryClip = ClipData.newPlainText("conversion num", text)
 
-        Toast.makeText(this, R.string.clipboard, Toast.LENGTH_SHORT).show()
+        toast(R.string.msg_clipboard)
     }
 
-    fun String.safeToDouble() = try { toDouble() } catch (e: NumberFormatException) { Double.NaN }
+    private fun updateShortcuts() = configuration(fromSdk = 25) {
+
+        ShortcutUtils.setShortcuts(this, favourites)
+    }
+
+    private fun EditText.afterTextChanged(listener: (Editable?) -> Unit) {
+
+        textChangedListener { afterTextChanged(listener) }
+    }
 }
